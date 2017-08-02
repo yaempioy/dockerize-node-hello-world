05e8d03ba0ce75ceecf0c10f1150ab0df3e83e87
Kitti Boonchan <kornkittig@gmail.com>
2017-08-02 13:39:30 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const os = require('os');
+
+const PORT = 8080
+const app = express()
+app.get('/', (req, res) => {
+    res.send(`Hello World, I'm run on ${os.hostname()}`)
+})
+app.listen(PORT)
+console.log(`Running on http://localhost:${PORT}`)
