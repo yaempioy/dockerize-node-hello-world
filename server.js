179cf819ba0f33fa0625f020c4a6535a1addd627
Kitti Boonchan <korn@Kittis-MacBook-Pro.local>
2017-09-12 18:45:59 +0700

    Test CI

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const os = require('os');
 const PORT = 8080
 const app = express()
 app.get('/', (req, res) => {
-    res.send(`Hello Quant, I'm run on ${os.hostname()}`)
+    res.send(`Hello Veda Network, I'm run on ${os.hostname()}`)
 })
 app.listen(PORT)
 console.log(`Running on http://localhost:${PORT}`)
