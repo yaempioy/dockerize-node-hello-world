0d7de0ccf5e57f4f51c41b7f37b9949a7301296d
Kitti Boonchan <korn@Kittis-MacBook-Pro.local>
2017-09-15 11:56:17 +0700

    Integration Jenkins notification with Slack

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const os = require('os');
 const PORT = 8080
 const app = express()
 app.get('/', (req, res) => {
-    res.send(`Hello Final test Jenkins CI/CD, Docker Registry, Rancher Compose CLI, Rancher , I'm run on ${os.hostname()}`)
+    res.send(`Final test Jenkins CI/CD, Docker Registry, Rancher Compose CLI, Rancher -> Jenkins Notification, I'm run on ${os.hostname()}`)
 })
 app.listen(PORT)
 console.log(`Running on http://localhost:${PORT}`)
